08e54beb2ee62f96d80d5124281cb2aca144293a
fullstacksdd <fullstacksdd@gmail.com>
2022-02-24 20:36:32 +0000

    Create Customer functionality

---
+++ admin-demo/src/main/java/com/admin/demo/controller/CustomerController.java
@@ -0,0 +1,79 @@
+package com.admin.demo.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.admin.demo.exception.ResourceNotFoundException;
+import com.admin.demo.model.Customer;
+import com.admin.demo.repository.CustomerRepository;
+import com.admin.demo.service.SequenceGeneratorService;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
+public class CustomerController {
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private SequenceGeneratorService sequenceGeneratorService;
+
+	@GetMapping("/customers")
+	public List<Customer> getAllCustomers() {
+		return customerRepository.findAll();
+	}
+
+	@GetMapping("/customers/{id}")
+	public ResponseEntity<Customer> getCustomerById(@PathVariable(value = "id") Long customerId)
+			throws ResourceNotFoundException {
+		Customer customer = customerRepository.findById(customerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer not found for this id :: " + customerId));
+		return ResponseEntity.ok().body(customer);
+	}
+
+	@PostMapping("/customers")
+	public Customer createCustomer(@Valid @RequestBody Customer customer) {
+		customer.setId(sequenceGeneratorService.generateSequence(Customer.SEQUENCE_NAME));
+		return customerRepository.save(customer);
+	}
+
+	@PutMapping("/customers/{id}")
+	public ResponseEntity<Customer> updateCustomer(@PathVariable(value = "id") Long customerId,
+			@Valid @RequestBody Customer customerDetails) throws ResourceNotFoundException {
+		Customer customer = customerRepository.findById(customerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer not found for this id :: " + customerId));
+
+		customer.setEmailId(customerDetails.getEmailId());
+		customer.setLastName(customerDetails.getLastName());
+		customer.setFirstName(customerDetails.getFirstName());
+		final Customer updatedCustomer = customerRepository.save(customer);
+		return ResponseEntity.ok(updatedCustomer);
+	}
+
+	@DeleteMapping("/customers/{id}")
+	public Map<String, Boolean> deleteCustomer(@PathVariable(value = "id") Long customerId)
+			throws ResourceNotFoundException {
+		Customer customer = customerRepository.findById(customerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer not found for this id :: " + customerId));
+
+		customerRepository.delete(customer);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+	}
+}
